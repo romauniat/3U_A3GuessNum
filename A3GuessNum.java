6e9a24b782137dd46bd486332bbba8a679e28452
romauniat <92386260+romauniat@users.noreply.github.com>
2021-11-22 15:24:13 -0500

    Update A3GuessNum.java

--- A3GuessNum.java
+++ A3GuessNum.java
@@ -1,13 +1,27 @@
-/*Name:
+/*
+*Name:
 *Date:
 *Desription:
 */
 
+import java.util.*;
 
 public A3GuessNum{
+  
+  
+  //main method
   public static void main(String[] arg){
+    // Make a scanner and Random Object.
+    Scanner input = new Scanner(System.in);
+    Random numGenerator = new Random();
     
-  
-  
-  }
+    // Generate a random number from 0-5.
+    int answer = numGenerator.nextInt(6);
+    int guess = -1; // default guess num.
+    
+    System.out.println("Guess a number");
+    int guess = input.nextInt();  
+    
+      
+  }// close main
 }
