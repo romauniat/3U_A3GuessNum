64a84b51f9f4c49f418ef81b12a47d7f9f9d3dc1
TuongLuu <73666044+luut189@users.noreply.github.com>
2021-11-24 01:27:48 +0000

    Added difficulty.

--- A3GuessNum.java
+++ A3GuessNum.java
@@ -15,7 +15,7 @@ public class A3GuessNum {
   //Boolean for the game loop
   static boolean running = true;
 
-  static int in, state, score; //int for user input and state in play() method
+  static int in, state, diff, score; //int for (user input, state for play() method, difficulty, and score)
 
   //int for bound
   static int lowBound = 0;
@@ -31,18 +31,26 @@ public class A3GuessNum {
   //main method
   public static void main(String[] arg){
     state = 1; //set state of play() method to 1
+    
+    System.out.print("Choose your difficulty (1-Easy, 2-Hard): ");
+    diff = input.nextInt();
 
     //game loop
     while(running) {
-      play();
+      if(diff == 1) {
+        playEasy();
+      } else if(diff == 2) {
+        playHard();
+      }
     }
 
   }// close main
 
-  public static void play() {
+  public static void playEasy() {
     //check if state is 1 and if there is any live
+    int num = -1;
     if(state == 1 && live > 0) {
-      int num = rand.nextInt(highBound - lowBound) + lowBound; //generate random number
+      num = rand.nextInt(highBound - lowBound) + lowBound; //generate random number
       
       int hint; //declare hint for later use
       
@@ -83,8 +91,8 @@ public class A3GuessNum {
         }
       }
 
-      //if wins and still has lives left
-      if(in == num && live > 0) {
+      //if wins and still has lives left (or one left)
+      if(in == num && live >= 0) {
         System.out.println("\nCongrats! You are right");
         System.out.println("The number is: " + num);
         System.out.println("Do you want to continue?");
@@ -94,7 +102,7 @@ public class A3GuessNum {
     }
 
     //if loses
-    if(live == 0 && state == 1) {
+    if(live == 0 && state == 1 && in != num) {
       System.out.println("You Lose!");
       System.out.println("Do you want to continue?");
       state = 0;
@@ -109,6 +117,76 @@ public class A3GuessNum {
         highBound = 100;
         live = INITIAL_LIVE;
         System.out.println("Loading...\n");
+        System.out.print("Choose your difficulty (1-Easy, 2-Hard): ");
+        diff = input.nextInt();
+        state = 1;
+      } else if(ans.equals("No")) {
+        System.out.println("\nScores: " + score);
+        System.out.println("END");
+        running = false;
+      }
+    }
+  }
+
+  public static void playHard() {
+    int num = -1;
+    //check if state is 1 and if there is any live
+    if(state == 1 && live > 0) {
+      num = rand.nextInt(highBound - lowBound) + lowBound; //generate random number
+      
+      //ask user for input
+      System.out.print("Guess a number: ");
+      check(); //call check() method
+
+      //loop until user get the right number
+      while(in != num && live > 0) {
+        //if user's number is less than the generated number
+        if(in < num) {
+          lowBound = in;
+          System.out.println("\nLives left: " + live);
+          System.out.println("Current range: (" + lowBound + "-" + highBound + ")");
+          System.out.print("Enter a new number: ");
+          check();
+        }
+
+        //if user's number is more than the generated number
+        else if(in > num) {
+          highBound = in;
+          System.out.println("\nLives left: " + live);
+          System.out.println("Current range: (" + lowBound + "-" + highBound + ")");
+          System.out.print("Enter a new number: ");
+          check();
+        }
+      }
+
+      //if wins and still has lives left (or one left)
+      if(in == num && live >= 0) {
+        System.out.println("\nCongrats! You are right");
+        System.out.println("The number is: " + num);
+        System.out.println("Do you want to continue?");
+        score += 1;
+        state = 0;
+      } 
+    }
+
+    //if loses
+    if(live == 0 && state == 1 && in != num) {
+      System.out.println("You Lose!");
+      System.out.println("Do you want to continue?");
+      state = 0;
+    }
+
+    //ask if user want to continue the game
+    if(state == 0) {
+      ans = input.nextLine();
+      if(ans.equals("Yes")) {
+        running = true;
+        lowBound = 0;
+        highBound = 100;
+        live = INITIAL_LIVE;
+        System.out.println("Loading...\n");
+        System.out.print("Choose your difficulty (1-Easy, 2-Hard): ");
+        diff = input.nextInt();
         state = 1;
       } else if(ans.equals("No")) {
         System.out.println("\nScores: " + score);
@@ -120,7 +198,7 @@ public class A3GuessNum {
 
   //method to check for input validation
   public static void check() {
-    
+
     //check if user didn't type an int
     while(!input.hasNextInt()) {
       System.out.println("That is not a number!");
